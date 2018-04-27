82b5134ce3f437774908fd5f4ea5bda4f72cd2fc
rraya <rraya@nearsoft.com>
2018-04-27 00:13:18 -0700

    Adding service

--- app/src/main/java/rraya/nearsoft/com/pokedex/PokeDexMain.kt
+++ app/src/main/java/rraya/nearsoft/com/pokedex/PokeDexMain.kt
@@ -23,63 +23,10 @@ class PokeDexMain : AppCompatActivity() {
                 .build()
     }
 
-    private val pokemonRV by lazy { findViewById<RecyclerView>(R.id.recyclerView) }
-    private var pokemonListAdapter = PokemonAdapter()
-
-    private var offset: Int = 0
-    private var loading = true
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poke_dex_main)
-        pokemonRV.adapter = pokemonListAdapter
-        val layoutManager = GridLayoutManager(this, 3)
-        pokemonRV.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            private var firstVisibleItem = 0
-            private var visibleItemCount = 0
-            private var totalItemCount = 0
-            private var pastVisibleItems = 0
-
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                if (dy > 0) {
-                    visibleItemCount = recyclerView.childCount
-                    totalItemCount = layoutManager.itemCount
-                    firstVisibleItem = layoutManager.findFirstVisibleItemPosition()
-                    pastVisibleItems = layoutManager.findFirstVisibleItemPosition()
-
-                    if (loading) {
-                        if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
-                            loading = false
-                            offset += 20
-                            obtainPokemon(offset)
-                        }
-                    }
-                }
-            }
-        })
-
-        pokemonRV.layoutManager = layoutManager
-
-        loading = true
-        obtainPokemon(offset)
     }
 
-    private fun obtainPokemon(offset: Int) {
-        val service = retrofit.create(PokeAPI::class.java)
-
-        service.getPokemnonList(20, offset)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { result ->
-                            loading = true
-                            val pokemonResponse = result.results
-                            pokemonListAdapter.addPokemons(pokemonResponse)
-                        },
-                        { error -> loading = true }
-                )
-    }
 }
